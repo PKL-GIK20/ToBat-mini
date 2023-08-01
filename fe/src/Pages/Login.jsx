3a32c12f7f25968a2b0c22076cd7a24b902cfeaf
Gefi Sanjaya <76405521+gefisanjaya@users.noreply.github.com>
2023-08-01 13:14:39 +0700

    skeleton and login page

---
+++ fe/src/Pages/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="flex justify-center h-screen">
+            <div className="flex rounded-lg overflow-hidden shadow-md w-[80%] h-[80%] m-[5%]">
+                <div className="bg-primary p-4 w-1/2 flex justify-center">
+                    <p>a</p>
+                </div>
+                <div className="bg-white p-4 w-1/2 flex justify-center">
+                    <div>
+                        
+                    </div>
+                    <form>
+                        <div className='flex flex-col font-montserrat'>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Login;
