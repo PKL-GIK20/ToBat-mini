1558d8b5e622f6099765081d51956e4ee2dd26c4
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2023-08-03 08:40:51 +0700

    login

--- backend/src/routes/stock.js
+++ backend/src/routes/stock.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const stockController = require('../controllers/stock');
 
 // Menampilkan stok untuk suatu produk berdasarkan ID produk
-router.get('/products/:productId/stock', stockController.getProductStock);
+router.get('/:productId/stock', stockController.getProductStock);
 
 // Menambahkan stok baru untuk suatu produk
-router.post('/products/:productId/stock', stockController.addProductStock);
+router.post('/:productId/stock', stockController.addProductStock);
 
 // Controller lainnya sesuai kebutuhan, seperti mengubah data stok, atau menghapus stok.
 
